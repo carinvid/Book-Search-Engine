eac2e29a11ecfd4a247f9e797372d9dd5df33fa7
Carolina Reedy <carinvid@gmail.com>
2021-09-25 23:48:13 -0600

    mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb+srv://@cluster0.ygwwm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/booksearch",
+  {
+    dbName: "booksearch",
+    user: "carinvid",
+    pass: "FUhyM45VpNNkuf0B",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
